012fb46df7ab8043d1d45298566c8658aa4c8f7a
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2020-09-14 23:34:15 +0530

    Create area.sql

---
+++ area.sql
@@ -0,0 +1,10 @@
+INSERT INTO AREA VALUES('B1006', 'Calicut', 'Kerala');
+INSERT INTO AREA VALUES('B1007', 'Kollam', 'Kerala');
+INSERT INTO AREA VALUES('B1008', 'Ernakulam', 'Kerala');
+INSERT INTO AREA VALUES('B1009', 'Calicut', 'Kerala');
+INSERT INTO AREA VALUES('B1010', 'Kollam', 'Kerala');
+INSERT INTO AREA VALUES('A2001', 'Mumbai', 'Maharashtra');
+INSERT INTO AREA VALUES('A2002', 'Nagpur', 'Maharashtra');
+INSERT INTO AREA VALUES('A2003', 'Pune', 'Maharashtra');
+INSERT INTO AREA VALUES('A2004', 'Mumbai', 'Maharashtra');
+INSERT INTO AREA VALUES('A2005', 'Nagpur', 'Maharashtra');
